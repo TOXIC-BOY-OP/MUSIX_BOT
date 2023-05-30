fa37ac35ca6c503c460f4a308aec66eaeb32461b
TOXIC-BOY-OP <134930342+TOXIC-BOY-OP@users.noreply.github.com>
2023-05-30 15:08:13 +0530

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-API_ID = int(os.getenv("API_ID"))
+API_ID = int(os.getenv("25374274"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
-OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
+DATABASE_URL = os.getenv("mongodb+srv://hnyx:wywyw2@cluster0.9dxlslv.mongodb.net/?retryWrites=true&w=majority")
+OWNER_ID = list({int(x) for x in os.environ.get("6109531260", "").split()})
